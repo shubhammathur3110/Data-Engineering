0325723c1f348f58cfafad81e5ba5232c9d06895
shubhammathur3110 <41752221+shubhammathur3110@users.noreply.github.com>
2022-10-26 12:32:39 +0530

    add new on 26-10-22

--- images/pythonsql/deploy.sh
+++ images/pythonsql/deploy.sh
@@ -3,7 +3,7 @@
 if [ ! -e /opt/.initialized ]
 then
   sudo touch /opt/.initialized
-#  sudo chown -R shubham:shubham /home/shubhammathur3110/material
+#  sudo chown -R shubham:shubham /home/shubham/material
 fi
 
-/home/shubhammathur3110/.local/bin/jupyter lab --ip 0.0.0.0
+/home/shubham/.local/bin/jupyter lab --ip 0.0.0.0
